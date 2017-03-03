7e5e7ec2f777925f00cdea2052615638288a27ab
Michael Tiller <michael.tiller@gmail.com>
2017-03-02 20:57:27 -0500

    Presentation rendering with Spectacle, FuseBox and TypeScript

--- src/presentation.tsx
+++ src/presentation.tsx
@@ -1,19 +1,21 @@
 import React = require('react');
-import { Deck, Heading } from 'spectacle';
+import { Deck, Heading, Slide } from 'spectacle';
 
 export class Presentation extends React.Component<void, void> {
     render() {
         return (
             <Deck>
-                <Heading size={1} fit caps lineHeight={1} textColor="black">
-                    Spectacle
+                <Slide>
+                    <Heading size={1} fit caps lineHeight={1} textColor="black">
+                        Spectacle
                 </Heading>
-                <Heading size={1} fit caps>
-                    A ReactJS Presentation Library
+                    <Heading size={1} fit caps>
+                        A ReactJS Presentation Library
                 </Heading>
-                <Heading size={1} fit caps textColor="black">
-                    Where You Can Write Your Decks In JSX
+                    <Heading size={1} fit caps textColor="black">
+                        Where You Can Write Your Decks In JSX
                 </Heading>
+                </Slide>
             </Deck>
         );
     }
